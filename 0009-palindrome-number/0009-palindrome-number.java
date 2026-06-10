abbd4fbeb1af4b2e9133a63425a127962f50d317
dsrividya <134177366+dsrividya@users.noreply.github.com>
2026-06-09 22:18:44 -0500

    Time: 8 ms (9.69%), Space: 45.5 MB (96.2%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -3,14 +3,10 @@ class Solution {
         if (x < 0) {
             return false;
         }
-        int n=x;
-        int digit=0;
-        while(x>0)
-        {
-            int temp=x%10;
-            digit=digit*10+temp;
-            x=x/10;
-        }
-       return (n==digit);
+
+        String original = String.valueOf(x);
+        String reversed = new StringBuilder(original).reverse().toString();
+
+        return original.equals(reversed);
     }
 }
